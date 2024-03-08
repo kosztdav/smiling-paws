a22bb8feca01d001488ed21dc62e21675772986a
VorosDominik <medoxidhun@gmail.com>
2024-03-08 22:18:45 +0100

    Header hiba javitás

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,18 +3,23 @@ import './Header.css';
 
 const Header = () => (
 	<div className="header">
-
-		<h1>Mosolygó mancsok	 medoxid</h1>
-
-		<img className="languageChooser" src="src\Assets\tilt 1.png" alt="Placeholder Image" />
-		<h1>Mosolygó<br/> mancsok</h1>
-		<img className='hero' src="src\Assets\banner1.png" alt="Placeholder Image" />
-		<p className='hero-text'>Fedezd fel foglalkozásainkat és<br/> csatlakozz valemelyikhez!</p>
-		<a className='heroBookmark'><img src='src\Assets\nyil.svg' alt=""/>Felzedezés</a>
+		<img className="title" src="public\Assets\HeroTitle.png" alt="Placeholder Image" />
+		<h1>
+			Mosolygó
+			<br /> mancsok
+		</h1>
+		<img className="hero" src="public\Assets\HeroBanner.png" alt="Placeholder Image" />
+		<p className="hero-text">
+			Fedezd fel foglalkozásainkat és
+			<br /> csatlakozz valemelyikhez!
+		</p>
+		<a className="heroBookmark">
+			<img src="public\Assets\nyil.svg" alt="" />
+			Felzedezés
+		</a>
 
 		<LanguageSelector />
-		<img className="wawes" src="src\Assets\wavesOpacity 1.png" alt="Placeholder Image" />
-		
+		<img className="wawes" src="/public\Assets\HeroWawe.png" alt="Placeholder Image" />
 	</div>
 );
 
